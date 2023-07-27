147d46cc4f508a87b5ac2e9c7c2f6b0fdeeb68c3
Junaid Ahmed <junaid.ahmed@postnord.com>
2023-07-28 00:06:40 +0200

    adding databse cred as provider to next auth

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,40 +1,35 @@
-import type { NextAuthOptions  } from "next-auth";
+import prismadb from "@/lib/prismadb";
+import type { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials"
 
+import * as bcrypt from 'bcrypt';
+
 export const options: NextAuthOptions = {
-    providers: [
-        CredentialsProvider({
-          // The name to display on the sign in form (e.g. 'Sign in with...')
-          name: 'Credentials',
-          // The credentials is used to generate a suitable form on the sign in page.
-          // You can specify whatever fields you are expecting to be submitted.
-          // e.g. domain, username, password, 2FA token, etc.
-          // You can pass any HTML attribute to the <input> tag through the object.
-          credentials: {
-            username: { label: "Username", type: "text", placeholder: "jsmith" },
-            password: { label: "Password", type: "password" }
-          },
-          async authorize(credentials, req) {
-            // You need to provide your own logic here that takes the credentials
-            // submitted and returns either a object representing a user or value
-            // that is false/null if the credentials are invalid.
-            // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-            // You can also use the `req` object to obtain additional parameters
-            // (i.e., the request IP address)
-            const res = await fetch("/api/login", {
-              method: 'POST',
-              body: JSON.stringify(credentials),
-              headers: { "Content-Type": "application/json" }
-            })
-            const user = await res.json()
-      
-            // If no error and we have user data, return it
-            if (res.ok && user) {
-              return user
-            }
-            // Return null if user data could not be retrieved
-            return null
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: 'Credentials',
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        const user = await prismadb.user.findFirst({
+          where: {
+            email: credentials?.username
           }
         })
-      ] 
+        if (user != null && credentials?.password != null && await bcrypt.compare(credentials.password, user.password,)) {
+          const { password, ...userWithOutPassword } = user;
+          return userWithOutPassword;
+        } else {
+          return null;
+        }
+      }
+    })
+  ]
 }
---
+++ src/middleware.ts
@@ -0,0 +1,6 @@
+export {default} from 'next-auth/middleware';
+
+
+
+
+
